ec794cdd704a01bae4e5e0883940555768ee99c1
RE. Wilson <121303360+RW2023@users.noreply.github.com>
2023-10-30 06:50:50 -0400

    create quote generator

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,8 +1,26 @@
+'use client';
+import { useState } from 'react';
+import BlackHistoryQuoteGenerator from '@/components/BlackHistoryQuoteGenerator';
 
 export default function Home() {
-  return (
-   
-      <h1 className="text-center text-headline text-xl p-2 mx-0">Home</h1>
+  const [quote, setQuote] = useState('');
+
+  interface QuoteData {
+    content: string; // Assuming the quote content is a string
+  }
   
-  )
+
+  const handleNewQuote = async () => {
+    const response = await fetch('https://api.quotable.io/random?tags=history');
+    const data: QuoteData = await response.json(); // Type annotation to ensure compatibility with QuoteData interface
+    setQuote(data.content);
+  };
+  
+
+  return (
+    <div>
+      <h1 className="text-center text-headline text-xl p-2 mx-0">Home</h1>
+      <BlackHistoryQuoteGenerator quote={quote} handleNewQuote={handleNewQuote} />
+    </div>
+  );
 }
---
+++ src/data/blackHistoryQuotes.jsx
@@ -0,0 +1,117 @@
+// File: src/data/blackHistoryQuotes.js
+const blackHistoryQuotes = [
+  {
+    id: '1',
+    author: 'Martin Luther King Jr.',
+    quote: 'The time is always right to do what is right.',
+  },
+  {
+    id: '2',
+    author: 'Harriet Tubman',
+    quote: 'Every great dream begins with a dreamer.',
+  },
+  {
+    id: '3',
+    author: 'Nelson Mandela',
+    quote:
+      'The greatest glory in living lies not in never falling, but in rising every time we fall.',
+  },
+  {
+    id: '4',
+    author: 'Maya Angelou',
+    quote:
+      "I've learned that people will forget what you said, people will forget what you did, but people will never forget how you made them feel.",
+  },
+  {
+    id: '5',
+    author: 'Malcolm X',
+    quote:
+      'Education is the passport to the future, for tomorrow belongs to those who prepare for it today.',
+  },
+  {
+    id: '6',
+    author: 'James Baldwin',
+    quote:
+      'Not everything that is faced can be changed, but nothing can be changed until it is faced.',
+  },
+  {
+    id: '7',
+    author: 'Rosa Parks',
+    quote:
+      "I have learned over the years that when one's mind is made up, this diminishes fear.",
+  },
+  {
+    id: '8',
+    author: 'Frederick Douglass',
+    quote: 'It is easier to build strong children than to repair broken men.',
+  },
+  {
+    id: '9',
+    author: 'Sojourner Truth',
+    quote: 'Truth is powerful and it prevails.',
+  },
+  {
+    id: '10',
+    author: 'Thurgood Marshall',
+    quote:
+      'In recognizing the humanity of our fellow beings, we pay ourselves the highest tribute.',
+  },
+  {
+    id: '11',
+    author: 'W.E.B. Du Bois',
+    quote: 'The cost of liberty is less than the price of repression.',
+  },
+  {
+    id: '12',
+    author: 'Marcus Garvey',
+    quote: 'With confidence, you have won before you have started.',
+  },
+  {
+    id: '13',
+    author: 'Audre Lorde',
+    quote:
+      'I am not free while any woman is unfree, even when her shackles are very different from my own.',
+  },
+  {
+    id: '14',
+    author: 'Booker T. Washington',
+    quote:
+      'Success is to be measured not so much by the position that one has reached in life as by the obstacles which he has overcome.',
+  },
+  {
+    id: '15',
+    author: 'Langston Hughes',
+    quote:
+      'Hold fast to dreams, for if dreams die, life is a broken-winged bird that cannot fly.',
+  },
+  {
+    id: '16',
+    author: 'Shirley Chisholm',
+    quote:
+      "You don't make progress by standing on the sidelines, whimpering and complaining. You make progress by implementing ideas.",
+  },
+  {
+    id: '17',
+    author: 'Ida B. Wells',
+    quote: 'The way to right wrongs is to turn the light of truth upon them.',
+  },
+  {
+    id: '18',
+    author: 'Angela Davis',
+    quote:
+      'I am no longer accepting the things I cannot change. I am changing the things I cannot accept.',
+  },
+  {
+    id: '19',
+    author: 'George Washington Carver',
+    quote: 'Education is the key to unlock the golden door of freedom.',
+  },
+  {
+    id: '20',
+    author: 'Carter G. Woodson',
+    quote:
+      'Those who have no record of what their forebears have accomplished lose the inspiration which comes from the teaching of biography and history.',
+  },
+];
+
+export default blackHistoryQuotes;
