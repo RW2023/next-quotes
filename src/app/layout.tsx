170f96df3c2f5360327286b86aad3711a6ab82bc
RE. Wilson <121303360+RW2023@users.noreply.github.com>
2023-10-31 08:49:55 -0400

    update meta data

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,8 +2,8 @@ import type { Metadata } from 'next'
 import './globals.css'
 
 export const metadata: Metadata = {
-  title: 'Quotable Quotes',
-  description: 'Random quotes from historical figures',
+  title: 'Historical Black Thought Leaders',
+  description: 'Random quotes from historical black figures',
 }
 
 export default function RootLayout({
@@ -12,7 +12,7 @@ export default function RootLayout({
   children: React.ReactNode
 }) {
   return (
-    <html lang="en" data-theme='forest'>
+    <html lang="en" data-theme='halloween'>
       <body>{children}</body>
     </html>
   )
