90df307e891f08e4d4319372b645e6fd6272d242
RE. Wilson <121303360+RW2023@users.noreply.github.com>
2023-11-01 07:13:40 -0400

    use dalle to create og image add to _document.tsx

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -10,6 +10,16 @@ class MyDocument extends Document {
             href="https://fonts.googleapis.com/css2?family=Poppins:wght@400;700&family=Roboto+Slab:wght@400;700&display=swap"
             rel="stylesheet"
           />
+          <meta
+            property="og:image"
+            content="https://next-quotes-pied.vercel.app/OG.png"
+          />
+          <meta property="og:image:width" content="1200" />
+          <meta property="og:image:height" content="630" />
+          <meta
+            property="og:image:alt"
+            content="Quotes from African American Thought Leaders"
+          />
         </Head>
         <body>
           <Main />
