cbe57ac0353761ee256e291ce0ef40f2cc031729
RE. Wilson <121303360+RW2023@users.noreply.github.com>
2023-11-01 16:08:24 -0400

    update og image link

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -12,7 +12,7 @@ class MyDocument extends Document {
           />
           <meta
             property="og:image"
-            content="https://next-quotes-pied.vercel.app/OG.png"
+            content="https://next-quotes-pied.vercel.app/og.png"
           />
           <meta property="og:image:width" content="1200" />
           <meta property="og:image:height" content="630" />
