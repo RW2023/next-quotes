3ba7c677e05fdae10daa6a2154ed40f429ada460
RE. Wilson <121303360+RW2023@users.noreply.github.com>
2023-11-01 09:58:44 -0400

    update Footer add link to current port site

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -7,9 +7,21 @@ interface Props {}
 
 const Footer: FC<Props> = (props): JSX.Element => {
     return (
-        <div className='text-center'>
-            <p>&copy; 2023 A black Developer. All rights reserved.</p>
-        </div>
+      <div className="text-center">
+        <p>
+          &copy; 2023 
+          <span className="font-sans font-bold text-highlight px-2">
+            <a
+              href="https://nextport-alpha.vercel.app/"
+              target="_blank"
+              rel="noopener noreferrer"
+            >
+              Ryan Wilson
+            </a>
+          </span>
+           A black Developer. All rights reserved.
+        </p>
+      </div>
     );
 };
 
