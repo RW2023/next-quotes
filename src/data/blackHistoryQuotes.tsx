804cbfa27cf7d7279ac7ce0bf73298e286396b63
RE. Wilson <121303360+RW2023@users.noreply.github.com>
2023-11-01 10:27:14 -0400

    add wikipedia links to images

--- src/data/blackHistoryQuotes.tsx
+++ src/data/blackHistoryQuotes.tsx
@@ -1,18 +1,21 @@
 // File: src/data/blackHistoryQuotes.js
 
-
 const blackHistoryQuotes = [
   {
     id: '1',
     author: 'Martin Luther King Jr.',
     quote: 'The time is always right to do what is right.',
     img: '/Martin_Luther_King,_Jr..jpg',
+    lifespan: '1929-1968',
+    wikiLink: 'https://en.wikipedia.org/wiki/Martin_Luther_King_Jr.',
   },
   {
     id: '2',
     author: 'Harriet Tubman',
     quote: 'Every great dream begins with a dreamer.',
     img: '/Harriet_Tubman.jpg',
+    lifespan: '1822-1913',
+    wikiLink: 'https://en.wikipedia.org/wiki/Harriet_Tubman',
   },
   {
     id: '3',
@@ -20,6 +23,8 @@ const blackHistoryQuotes = [
     quote:
       'The greatest glory in living lies not in never falling, but in rising every time we fall.',
     img: '/nelsonMandela.jpg',
+    lifespan: '1918-2013',
+    wikiLink: 'https://en.wikipedia.org/wiki/Nelson_Mandela',
   },
   {
     id: '4',
@@ -27,6 +32,8 @@ const blackHistoryQuotes = [
     quote:
       "I've learned that people will forget what you said, people will forget what you did, but people will never forget how you made them feel.",
     img: '/Angelou.jpg',
+    lifespan: '1928-2014',
+    wikiLink: 'https://en.wikipedia.org/wiki/Maya_Angelou',
   },
   {
     id: '5',
@@ -34,6 +41,8 @@ const blackHistoryQuotes = [
     quote:
       'Education is the passport to the future, for tomorrow belongs to those who prepare for it today.',
     img: '/Malcolm_X.jpg',
+    lifespan: '1925-1965',
+    wikiLink: 'https://en.wikipedia.org/wiki/Malcolm_X',
   },
   {
     id: '6',
@@ -41,6 +50,8 @@ const blackHistoryQuotes = [
     quote:
       'Not everything that is faced can be changed, but nothing can be changed until it is faced.',
     img: '/James_Baldwin.jpg',
+    lifespan: '1924-1987',
+    wikiLink: 'https://en.wikipedia.org/wiki/James_Baldwin',
   },
   {
     id: '7',
@@ -48,18 +59,24 @@ const blackHistoryQuotes = [
     quote:
       "I have learned over the years that when one's mind is made up, this diminishes fear.",
     img: '/Rosaparks.jpg',
+    lifespan: '1913-2005',
+    wikiLink: 'https://en.wikipedia.org/wiki/Rosa_Parks',
   },
   {
     id: '8',
     author: 'Frederick Douglass',
     quote: 'It is easier to build strong children than to repair broken men.',
     img: '/Frederick_Douglass.jpg',
+    lifespan: '1818-1895',
+    wikiLink: 'https://en.wikipedia.org/wiki/Frederick_Douglass',
   },
   {
     id: '9',
     author: 'Sojourner Truth',
     quote: 'Truth is powerful and it prevails.',
     img: '/Sojourner_Truth.jpg',
+    lifespan: '1797-1883',
+    wikiLink: 'https://en.wikipedia.org/wiki/Sojourner_Truth',
   },
   {
     id: '10',
@@ -67,18 +84,24 @@ const blackHistoryQuotes = [
     quote:
       'In recognizing the humanity of our fellow beings, we pay ourselves the highest tribute.',
     img: '/Thurgood-marshall.jpg',
+    lifespan: '1908-1993',
+    wikiLink: 'https://en.wikipedia.org/wiki/Thurgood_Marshall',
   },
   {
     id: '11',
     author: 'W.E.B. Du Bois',
     quote: 'The cost of liberty is less than the price of repression.',
     img: '/W.E.B._Du_Bois.jpg',
+    lifespan: '1868-1963',
+    wikiLink: 'https://en.wikipedia.org/wiki/W._E._B._Du_Bois',
   },
   {
     id: '12',
     author: 'Marcus Garvey',
     quote: 'With confidence, you have won before you have started.',
     img: '/marcusGarvey.jpg',
+    lifespan: '1887-1940',
+    wikiLink: 'https://en.wikipedia.org/wiki/Marcus_Garvey',
   },
   {
     id: '13',
@@ -86,6 +109,8 @@ const blackHistoryQuotes = [
     quote:
       'I am not free while any woman is unfree, even when her shackles are very different from my own.',
     img: '/Audre_Lorde.jpg',
+    lifespan: '1934-1992',
+    wikiLink: 'https://en.wikipedia.org/wiki/Audre_Lorde',
   },
   {
     id: '14',
@@ -93,6 +118,8 @@ const blackHistoryQuotes = [
     quote:
       'Success is to be measured not so much by the position that one has reached in life as by the obstacles which he has overcome.',
     img: '/Booker_T_Washington.jpg',
+    lifespan: '1856-1915',
+    wikiLink: 'https://en.wikipedia.org/wiki/Booker_T._Washington',
   },
   {
     id: '15',
@@ -100,6 +127,8 @@ const blackHistoryQuotes = [
     quote:
       'Hold fast to dreams, for if dreams die, life is a broken-winged bird that cannot fly.',
     img: '/LangstonHughes.jpg',
+    lifespan: '1902-1967',
+    wikiLink: 'https://en.wikipedia.org/wiki/Langston_Hughes',
   },
   {
     id: '16',
@@ -107,12 +136,16 @@ const blackHistoryQuotes = [
     quote:
       "You don't make progress by standing on the sidelines, whimpering and complaining. You make progress by implementing ideas.",
     img: '/Shirley_Chisholm.jpg',
+    lifespan: '1924-2005',
+    wikiLink: 'https://en.wikipedia.org/wiki/Shirley_Chisholm',
   },
   {
     id: '17',
     author: 'Ida B. Wells',
     quote: 'The way to right wrongs is to turn the light of truth upon them.',
     img: '/Ida_B._Wells-Barnett.jpg',
+    lifespan: '1862-1931',
+    wikiLink: 'https://en.wikipedia.org/wiki/Ida_B._Wells',
   },
   {
     id: '18',
@@ -120,12 +153,16 @@ const blackHistoryQuotes = [
     quote:
       'I am no longer accepting the things I cannot change. I am changing the things I cannot accept.',
     img: '/Angela_Davis.jpg',
+    lifespan: '1944-present',
+    wikiLink: 'https://en.wikipedia.org/wiki/Angela_Davis',
   },
   {
     id: '19',
     author: 'George Washington Carver',
     quote: 'Education is the key to unlock the golden door of freedom.',
     img: '/George_Washington_Carver.jpg',
+    lifespan: '1860s-1943',
+    wikiLink: 'https://en.wikipedia.org/wiki/George_Washington_Carver',
   },
   {
     id: '20',
@@ -133,6 +170,8 @@ const blackHistoryQuotes = [
     quote:
       'Those who have no record of what their forebears have accomplished lose the inspiration which comes from the teaching of biography and history.',
     img: '/Carter_G._Woodson.jpg',
+    lifespan: '1875-1950',
+    wikiLink: 'https://en.wikipedia.org/wiki/Carter_G._Woodson',
   },
 ];
 
