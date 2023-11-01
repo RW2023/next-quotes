5908472d3b2bea517fc935337f1271c22417c09b
RE. Wilson <121303360+RW2023@users.noreply.github.com>
2023-11-01 17:45:27 -0400

    create custom error page

---
+++ src/app/not-found.tsx
@@ -0,0 +1,29 @@
+//src/app/not-found.tsx
+import Link from 'next/link';
+
+function Custom404() {
+  return (
+    <div
+      className="min-h-screen flex items-center justify-center text-center bg-secondary"
+      data-theme="dark"
+    >
+      <div className="card bordered p-10 shadow-2xl bg-background">
+        <h1 className="text-4xl font-bold mb-4">
+          Oh No! That page is not here🥹
+        </h1>
+        <p className="mb-4">
+          The page you are looking for does not exist or an other error
+          occurred.
+        </p>
+        <Link
+          href="/"
+          className="underline-offset-1 bg-black text-headline btn hover:text-button"
+        >
+          Go back home
+        </Link>
+      </div>
+    </div>
+  );
+}
+
+export default Custom404;
