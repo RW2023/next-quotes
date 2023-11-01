ecd5b0d128eced38f96e197087a66efd9d49534f
RE. Wilson <121303360+RW2023@users.noreply.github.com>
2023-11-01 08:01:52 -0400

    correct root layout

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,27 +1,29 @@
-//src/app/layout.tsx
-import type { Metadata } from 'next'
-import './globals.css'
+// src/app/layout.tsx
+import { NextPage } from 'next';
+import './globals.css';
 import Navbar from '@/components/Navbar';
 import Footer from '@/components/Footer';
 
+interface Metadata {
+  title: string;
+  description: string;
+}
 
 export const metadata: Metadata = {
   title: 'Historical Black Thought Leaders',
   description: 'Random quotes from historical black figures',
-}
+};
 
-export default function RootLayout({
-  children,
-}: {
-  children: React.ReactNode
-}) {
+const RootLayout: NextPage<{ children: React.ReactNode }> = ({ children }) => {
   return (
     <html lang="en" data-theme="halloween">
       <body className="mt-5">
         <Navbar />
         {children}
-        </body>
-        <Footer />
+      </body>
+      <Footer />
     </html>
   );
-}
+};
+
+export default RootLayout;
