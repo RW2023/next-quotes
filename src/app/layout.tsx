7bc31a3d9fb3eeb0afd02de9803805a07f6ffe0e
RE. Wilson <121303360+RW2023@users.noreply.github.com>
2023-11-02 00:23:42 -0400

    Streamline OG metadata handling using metadata object

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -4,35 +4,46 @@ import './globals.css';
 import Navbar from '@/components/Navbar';
 import Footer from '@/components/Footer';
 import Head from 'next/head';
+import ogImage from '../../public/og.png'; 
 
 interface Metadata {
   title: string;
   description: string;
+  images: {
+    src: string;
+    width: number;
+    height: number;
+    alt: string;
+  }[];
 }
 
+// Your metadata object is also good, keep it as is
 export const metadata: Metadata = {
   title: 'Historical Black Thought Leaders',
   description: 'Random quotes from historical black figures',
+  images: [
+    { src: ogImage.src, width: 1200, height: 630, alt: 'OG Image Alt Text' },
+  ],
 };
 
 const RootLayout: NextPage<{ children: React.ReactNode }> = ({ children }) => {
   return (
     <html lang="en" data-theme="halloween">
       <Head>
-           <meta
-           
-            property="og:image"
-            content="https://next-quotes-pied.vercel.app/og.png"
-          />
-          <meta property="og:image:width" content="1200" />
-          <meta property="og:image:height" content="630" />
-          <meta
-            property="og:image:alt"
-            content="Quotes from African American Thought Leaders"
-          />
-          <meta property="og:title" content={metadata.title} />
-          <meta property="og:description" content={metadata.description} />
-          </Head>
+        {/* The following meta tags are streamlined using the metadata object */}
+        <meta property="og:image" content={metadata.images[0].src} />
+        <meta
+          property="og:image:width"
+          content={metadata.images[0].width.toString()}
+        />
+        <meta
+          property="og:image:height"
+          content={metadata.images[0].height.toString()}
+        />
+        <meta property="og:image:alt" content={metadata.images[0].alt} />
+        <meta property="og:title" content={metadata.title} />
+        <meta property="og:description" content={metadata.description} />
+      </Head>
       <body className="mt-5">
         <Navbar />
         {children}
