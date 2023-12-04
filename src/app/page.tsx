983148d5b71a36a6e3c5100de157a619fdd3c682
RE. Wilson <121303360+RW2023@users.noreply.github.com>
2023-12-04 07:57:39 -0500

    update page layout improve padding and margin on some elements edit content on page change grid gap to be smaller looked weird

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,12 +1,9 @@
-// File: src/pages/index.tsx
-
 'use client';
 
 import React, { useState } from 'react';
 import BlackHistoryQuoteGenerator from '@/components/BlackHistoryQuoteGenerator';
 import blackHistoryQuotes from '@/data/blackHistoryQuotes';
 import Image from 'next/image';
-
 
 export default function Home() {
   const [quote, setQuote] = useState(generateRandomQuote());
@@ -21,23 +18,30 @@ export default function Home() {
   };
 
   return (
-    <div className="min-h-screen bg-background ">
-      <div className="container mx-auto p-6 grid grid-cols-1 md:grid-cols-2 gap-6 bg-secondary shadow-lg rounded-lg pt-6 ">
-        <h1 className="col-span-full text-center font-bold text-4xl text-headline bg-black border-headline ">
+    <div className="min-h-screen bg-background">
+      <div className="container mx-auto px-4 md:px-0 p-6 grid grid-cols-1 md:grid-cols-2 gap-2 bg-secondary shadow-lg rounded-lg">
+        <h1 className="col-span-full text-center text-5xl font-bold text-headline">
           Timeless Wisdom from Black Thought Leaders
         </h1>
-        <h2 className="col-span-full text-center text-2xl font-bold mb-6 mt-1 text-tertiary">
+        <h2 className="col-span-full text-center text-3xl font-bold mb-4 mt-1 text-tertiary m-1">
           Wise Words from <span className="font-bold"> {quote.author} </span>
         </h2>
-        <div className="flex flex-col items-center p-3 bg-tertiary rounded-lg shadow-lg drop-shadow-lg">
-          <h2>With us from  <span className='text-headline from-secondary-content'>{quote.lifespan}</span></h2>
-          <a title='wikiLink' href={quote.wikiLink} target="_blank" rel="noopener noreferrer">
+        <div className="card bordered flex flex-col items-center p-3 bg-tertiary rounded-lg shadow-lg m-1">
+          <h2>
+            With us from <span className="text-headline">{quote.lifespan}</span>
+          </h2>
+          <a
+            title="wikiLink"
+            href={quote.wikiLink}
+            target="_blank"
+            rel="noopener noreferrer"
+          >
             <Image
               src={quote.img || '/fist.png'}
               alt={quote.author}
               height={300}
               width={300}
-              className="border-2 border-headline p-1 bg-tertiary rounded-lg shadow-lg mb-3"
+              className="rounded-box border-2 border-headline p-1"
             />
           </a>
           <div className="caption-bottom">
@@ -45,7 +49,7 @@ export default function Home() {
           </div>
           <h2 className="text-tertiary text-lg">{quote.author}</h2>
         </div>
-        <div className="flex flex-col items-center p-3 bg-tertiary rounded-lg shadow-lg">
+        <div className="card bordered flex flex-col items-center p-3 bg-tertiary rounded-lg shadow-lg m-2">
           <BlackHistoryQuoteGenerator
             quote={quote}
             handleNewQuote={handleNewQuote}
